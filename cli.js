5ae48c5cb2c95934766e3c57d80b0b770025e17d
Jon Schlinkert <dev@sellside.com>
2014-02-10 07:50:32 -0500

    v0.1.2
    improves CLI, adds success message

--- cli.js
+++ cli.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
+
 // Why more?
 require('./')();
+
+if(require('./package.json').version === require('./bower.json').version) {
+  var v = require('./bower.json').version;
+  console.log(chalk.green('>> Success.'), 'updated bower.json to ' + chalk.green(' v' + v));
+}
