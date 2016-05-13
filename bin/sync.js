d170e73149823b3f924d9ade79631664d7d0561c
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-13 15:54:16 -0400

    cli commands

--- bin/sync.js
+++ bin/sync.js
@@ -4,15 +4,29 @@ var path = require('path');
 var sync = require('../');
 var utils = require('../lib/utils');
 var argv = utils.minimist(process.argv.slice(2));
-
 var Composer = require('composer');
 var composer = new Composer();
 
 var Questions = require('question-cache');
 var questions = new Questions();
 
-var task = argv.bower || argv.new || (utils.exists('bower.json') ? ['bower']: ['create']);
 var cwd = argv.cwd ? path.resolve(argv.cwd) : process.cwd();
+
+var tasks = argv._.length ? argv._ : argv.bower || argv.new;
+if (typeof tasks === 'undefined') {
+  tasks = (utils.exists('bower.json') ? ['bower'] : ['bower-prompt']);
+}
+
+/**
+ * Read `bower.json` (if exists) and `package.json`
+ */
+
+composer.task('diff', function(cb) {
+  var bower = new utils.Bower(cwd);
+  var pkg = JSON.stringify(utils.pkg.sync(cwd), null, 2);
+  utils.diffJson(pkg, JSON.stringify(bower.data, null, 2));
+  cb();
+});
 
 /**
  * Read `bower.json` (if exists) and `package.json`
@@ -30,21 +44,12 @@ composer.task('bower', function(cb) {
  * Ask the user if they want to create a bower.json
  */
 
-composer.task('create', function(cb) {
+composer.task('bower-prompt', function(cb) {
   questions.confirm(this.name, 'bower.json does not exist, do you want to create one?');
   questions.ask(this.name, function(err, answers) {
     if (err) return cb(err);
-    if (answers.create === true) {
-      try {
-        var bower = new utils.Bower(cwd);
-        var pkg = utils.pkg.sync(cwd);
-        bower.data = sync(pkg, bower.data, argv);
-        bower.save();
-        cb();
-      } catch (err) {
-        console.error(err.stack);
-        cb(err);
-      }
+    if (answers['bower-prompt'] === true) {
+      composer.build('bower', cb);
     } else {
       cb();
     }
@@ -55,7 +60,7 @@ composer.task('create', function(cb) {
  * Run the specified "task"
  */
 
-composer.build(task, function(err) {
+composer.build(tasks, function(err) {
   if (err) {
     console.error(err);
     process.exit(1);
